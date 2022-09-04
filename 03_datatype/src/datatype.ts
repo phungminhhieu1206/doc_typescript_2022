91b41c16aac5d76edf405dbb4698a2b55f91206c
Phung Minh Hieu <hieuphung1206@Phungs-MacBook-Pro.local>
2022-09-04 23:20:11 +0700

    interface IContact

---
+++ 03_datatype/src/datatype.ts
@@ -0,0 +1,11 @@
+// Primitive types
+
+const myName: string = "Phung Hieu";
+const age: number = 22;
+const isFree: boolean = false;
+
+// Một số kiểu vừa và kiểu vừa là giá trị !
+const someVar: undefined = undefined;
+const anotherVar: null = null;
+
+console.log(typeof myName);
